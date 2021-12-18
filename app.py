60afda12cabd75f74376c46b5e98ad262a2d3429
Tomer Holzman <tomerxh@gmail.com>
2021-12-18 19:31:54 +0200

    changes

--- app.py
+++ app.py
@@ -7,31 +7,35 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/')
 def home_func():  # put application's code here
-    return render_template('Index.html')
-
+    found = False
+    if found:
+     return render_template('Index.html', name1='Tomer')
+    else:
+        return render_template('Index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
 @app.route('/about')
 def about_func(): # return about page
     name = 'Tomer'
+    #dicionary
     profile={'name': 'Guy', 'university':'second_name'}
-    degrees=['BSc','MSc']
-    hobbies=('art','surf','music')
+    degrees=['BSc','MSc','dr']
+    hobbies=('art','surf','music','drive')
     university = 'BGU'
     second_name = 'Tom'
     return render_template('about.html', name='Yuval'
-        ,second_name=second_name, uni=university
-        ,profile=profile,degrees=degrees,hobbies=hobbies)
+        ,second_name=second_name, uni=university,profile=profile,degrees=degrees,hobbies=hobbies)
 
 
-
-@app.route('/catalogs')
+@app.route('/Catalog')
 def catalog_func():
-    return "Welcome to catalog page"
+    return render_template(('Catalog.html'))
+
+
+
+
 
     #todo
     #url_for - calling func
