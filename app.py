5da3850e108827413285b2cec08874b85d3626c8
Tomer Holzman <tomerxh@gmail.com>
2021-12-20 22:28:33 +0200

    changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, url_for
 from flask import render_template
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/home_page')
 @app.route('/home')
@@ -13,29 +13,34 @@ def home_func():  # put application's code here
     else:
         return render_template('Index.html')
 
+
 if __name__ == '__main__':
     app.run(debug=True)
 
+
 @app.route('/about')
-def about_func(): # return about page
-    name = 'Tomer1'
-    #dicionary
-    profile={'name': 'Guy', 'university':'second_name'}
-    degrees=['BSc','MSc','dr']
-    hobbies=('art','surf','music','drive')
-    university = 'BGU'
-    second_name = 'Tomer'
-    return render_template('about.html', name='Yuval'
-                           ,second_name=second_name, uni=university,profile=profile,degrees=degrees,hobbies=hobbies)
+def about_func():  # return about page
+    return render_template('about.html')
 
 
 @app.route('/Catalog')
 def catalog_func():
     return render_template(('Catalog.html'))
 
+
 @app.route('/assignment8')
-def assigment8_func():
-    return render_template(('assignment8.html'))
+def ass_func():
+    name = 'Tomer'
+    # dicionary
+    profile = {'name': 'Guy', 'university': 'second_name'}
+    # list
+    degrees = ['BSc', 'MSc', 'dr']
+    # tupple
+    hobbies = ('art', 'surf', 'music', 'drive')
+    university = 'BGU'
+    second_name = 'Tom'
+    return render_template('assignment8.html', name='Yuval'
+                           , second_name=second_name, uni=university, profile=profile, degrees=degrees, hobbies=hobbies)
 
 
 @app.route('/Login')
@@ -47,9 +52,7 @@ def login_func():
 def signUp_func():
     return render_template(('Singup.html'))
 
-
-
-#todo
-#url_for - calling func
-#redirect -route
-#return redirect(url_for('catalog_func'))
+# todo
+# url_for - calling func
+# redirect -route
+# return redirect(url_for('catalog_func'))
